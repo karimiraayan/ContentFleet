d278099ef9c8cd49b2bc5ee98e6e0a4e85fe219b
mohammadali karimi <karimiraayan@gmail.com>
2024-02-24 13:16:25 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import reactLogo from './assets/react.svg'
+import Navbar from './components/Navbar'
+//import viteLogo from '/vite.svg'
+import './style/main.scss'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
