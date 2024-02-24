4d74a0f50b23f860a684715171d805826af45368
mohammadali karimi <karimiraayan@gmail.com>
2024-02-24 14:46:12 +0100

    add home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+  <section id="home">
+    <h1>
+      Home
+    </h1>
+    <p>
+      This is the home section
+    </p>
+  </section>
   )
 }
