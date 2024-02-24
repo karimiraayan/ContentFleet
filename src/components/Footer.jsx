a36475cd1475a63a83c041c91592717c8057cab2
mohammadali karimi <karimiraayan@gmail.com>
2024-02-24 15:47:45 +0100

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
 export default function Footer() {
   return (
-  <footer>
-    <p>Footer</p>
-  </footer>
-  )
+    <footer id="footer">
+      <div className="inner-layout">
+        <div className="row-1">
+          <div className="contact">
+            <h2>Contact</h2>
+            <p>
+              Email: <a href="mailto:test@test.de " />
+            </p>
+            <p>
+              Phone: <a href="tel:+1234567890" />
+            </p>
+          </div>
+        </div>
+      </div>
+
+        <p className="text-center">
+          {"\u00A9" + new Date().getFullYear() + " Mohammadali Karimi"}
+        </p>
+    </footer>
+  );
 }
